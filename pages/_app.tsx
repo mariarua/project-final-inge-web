becd5b797961d7af146fba8806490a8cc4241550
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-05-23 00:23:20 -0500

    Add google provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <SessionProvider session={pageProps.session}>
+    <Component {...pageProps} />
+  </SessionProvider>
+);
+
+export default App;
