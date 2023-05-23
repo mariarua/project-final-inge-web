c74372301b5335ca1352c5b877445b8c0abf900d
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-05-23 00:23:34 -0500

    format document

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import LoginBtn from '@/components/auth/LoginBtn'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
+const Home = () => (
     <>
       <Head>
         <title>Create Next App</title>
@@ -15,6 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <LoginBtn />
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -120,4 +121,5 @@ export default function Home() {
       </main>
     </>
   )
-}
+
+export default Home
