f53a2223bde81b761886c5f6fcc7ef4915cb0a42
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-05-23 20:55:20 -0500

    Add authOptions export

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import Auth0Provider from "next-auth/providers/auth0";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     Auth0Provider({
