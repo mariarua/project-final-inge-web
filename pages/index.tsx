a8cabb6de24b3167fd6dae5c5754685e9d82eb2e
mariarua <maria.rua1@udea.edu.co>
2023-05-28 00:02:02 -0500

    Add home to private routers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,36 @@
 import Layout from "@/layouts/Layout";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
-const Home = () => (
-  <>
-    <Head>
-      <title>Inventario</title>
-      <meta name="description" content="Gestión de inventarios" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    ()
-    <Layout>
-      <>
-        <h1 className="text-2xl font-bold underline">Hello world!</h1>
-      </>
-    </Layout>
-  </>
-);
+const Home = () => {
+  const router = useRouter();
+  const { status } = useSession();
 
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Inventario</title>
+        <meta name="description" content="Gestión de inventarios" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <>
+          <h1 className="text-2xl font-bold underline">Hello world!</h1>
+        </>
+      </Layout>
+    </>
+  );
+};
+
+Home.requireAuth = true;
 export default Home;
