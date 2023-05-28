493e951fe92a1eaa1d0523eaf6bf97ba0cecada5
mariarua <maria.rua1@udea.edu.co>
2023-05-28 00:35:11 -0500

    Add private router

--- pages/usuarios/index.tsx
+++ pages/usuarios/index.tsx
@@ -1,69 +1,85 @@
 import Layout from "@/layouts/Layout";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 
-const Materials = () => (
-  <>
-    <Head>
-      <title>Materiales</title>
-      <meta name="description" content="Gestión de materiales" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Layout>
-      <div className="flex items-center flex-col px-[80px] pt-[48px] h-screen gap-[120px]">
-        <span className="text-5xl">Gestión de materiales</span>
-        <div className="w-full">
-          <div className="flex flex-col gap-[40px]">
-            <div className="flex justify-end">
-              <span className="btn">Agregar material</span>
+const Materials = () => {
+  const router = useRouter();
+  const { status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (status === "unauthenticated") {
+    router.push("/login");
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Materiales</title>
+        <meta name="description" content="Gestión de materiales" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div className="flex items-center flex-col px-[80px] pt-[48px] h-screen gap-[120px]">
+          <span className="text-5xl">Gestión de materiales</span>
+          <div className="w-full">
+            <div className="flex flex-col gap-[40px]">
+              <div className="flex justify-end">
+                <span className="btn">Agregar material</span>
+              </div>
+              <table className="table-auto">
+                <thead>
+                  <tr>
+                    <th>Identificador</th>
+                    <th>Fecha de creación</th>
+                    <th>Nombre</th>
+                    <th>Saldo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Shining Star</td>
+                    <td>Malcolm Lockyer</td>
+                    <td>1961</td>
+                    <td>1961</td>
+                  </tr>
+                  <tr>
+                    <td>Witchy Woman</td>
+                    <td>The Eagles</td>
+                    <td>1972</td>
+                    <td>1972</td>
+                  </tr>
+                  <tr>
+                    <td>Shining Star</td>
+                    <td>Earth, Wind, and Fire</td>
+                    <td>1975</td>
+                    <td>1975</td>
+                  </tr>
+                  <tr>
+                    <td>Shining Star</td>
+                    <td>Malcolm Lockyer</td>
+                    <td>1961</td>
+                    <td>1961</td>
+                  </tr>
+                  <tr>
+                    <td>Shining Star</td>
+                    <td>Malcolm Lockyer</td>
+                    <td>1961</td>
+                    <td>1961</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th>Identificador</th>
-                  <th>Fecha de creación</th>
-                  <th>Nombre</th>
-                  <th>Saldo</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Shining Star</td>
-                  <td>Malcolm Lockyer</td>
-                  <td>1961</td>
-                  <td>1961</td>
-                </tr>
-                <tr>
-                  <td>Witchy Woman</td>
-                  <td>The Eagles</td>
-                  <td>1972</td>
-                  <td>1972</td>
-                </tr>
-                <tr>
-                  <td>Shining Star</td>
-                  <td>Earth, Wind, and Fire</td>
-                  <td>1975</td>
-                  <td>1975</td>
-                </tr>
-                <tr>
-                  <td>Shining Star</td>
-                  <td>Malcolm Lockyer</td>
-                  <td>1961</td>
-                  <td>1961</td>
-                </tr>
-                <tr>
-                  <td>Shining Star</td>
-                  <td>Malcolm Lockyer</td>
-                  <td>1961</td>
-                  <td>1961</td>
-                </tr>
-              </tbody>
-            </table>
           </div>
         </div>
-      </div>
-    </Layout>
-  </>
-);
+      </Layout>
+    </>
+  );
+};
 
+Materials.requireAuth = true;
 export default Materials;
