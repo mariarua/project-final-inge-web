5d319274dc1addfc7e6b1e0e9f63fc653cf048bd
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2023-05-31 13:52:28 -0500

    Add material creation service

--- components/modals/ModalMaterials.tsx
+++ components/modals/ModalMaterials.tsx
@@ -1,15 +1,58 @@
-import React, { Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import Modal from "./Modal";
+import { useMutation } from "@apollo/client";
+import { CREATE_MATERIAL } from "@/graphql/client/materials";
+import { toast } from "react-toastify";
 
 interface ModalMaterialsProps {
   openModalMaterials: boolean;
   setOpenModalMaterials: Dispatch<SetStateAction<boolean>>;
 }
 
-function ModalMaterials({
+interface FormData {
+  name: string;
+  price: number;
+}
+
+const initialFormData: FormData = {
+  name: "",
+  price: 0,
+};
+
+const ModalMaterials = ({
   openModalMaterials,
   setOpenModalMaterials,
-}: ModalMaterialsProps) {
+}: ModalMaterialsProps) => {
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+
+  const [createMaterial, { loading }] = useMutation(CREATE_MATERIAL);
+
+  const handleSubmit = async () => {
+    const { name, price } = formData;
+    try {
+      const { data } = await createMaterial({
+        variables: {
+          name,
+          price: Number(price),
+        },
+      });
+      const { createMaterial: material } = data;
+      toast.success(`${material.name} creado con éxito`);
+
+      setFormData(initialFormData);
+      setOpenModalMaterials(false);
+    } catch (error) {
+      toast.error("Error al crear el material");
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <Modal
       open={openModalMaterials}
@@ -18,28 +61,47 @@ function ModalMaterials({
     >
       <>
         <div className="flex flex-col gap-5">
-          <form action="">
+          <form>
             <label htmlFor="name">
               <span>Nombre:</span>
-              <input type="text" name="name" id="name" placeholder="Madera" />
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Madera"
+                onChange={handleChange}
+                value={formData.name}
+              />
             </label>
             <label htmlFor="price">
               <span>Saldo:</span>
-              <input type="text" name="price" id="price" placeholder="300" />
+              <input
+                type="number"
+                min={0}
+                step={1}
+                name="price"
+                id="price"
+                placeholder="300"
+                onChange={handleChange}
+                value={formData.price}
+              />
             </label>
           </form>
           <div className="flex justify-between">
-            <button onClick={() => setOpenModalMaterials(false)}>Salir</button>
-            <button>Enviar</button>
+            <button
+              disabled={loading}
+              onClick={() => setOpenModalMaterials(false)}
+            >
+              Salir
+            </button>
+            <button disabled={loading} onClick={handleSubmit}>
+              Enviar
+            </button>
           </div>
         </div>
       </>
     </Modal>
   );
-}
+};
 
 export default ModalMaterials;
-function createMaterial(arg0: { variables: { lot: any; bunches: any; collectionDate: any; }; refetchQueries: any[]; }) {
-  throw new Error("Function not implemented.");
-}
-
