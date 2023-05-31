fd96bab22e5dd822b134821c68fbda49f939f3ed
mariarua <maria.rua1@udea.edu.co>
2023-05-31 03:58:36 -0500

    Add link component to nav

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { IoIosLogOut } from "react-icons/io";
 import { useSession, signOut } from "next-auth/react";
-import PrivateComponent from "../privateComponent";
+import PrivateComponent from "@/components/privateComponent";
+import Link from "@/components/link";
 
 export const Sidebar = () => {
   const { data } = useSession();
@@ -23,17 +24,35 @@ export const Sidebar = () => {
           <IoIosLogOut onClick={() => signOut()} className="mb-2" />
         </div>
         <div className="w-full flex">
-          <nav className="w-full ">
+          <nav className="w-full">
             <ul className="w-full flex flex-col gap-2 text-xl font-thin tracking-[0.3em] mx-auto items-center">
-              <li className="w-full border-0 border-transparent text-gray-800 cursor-pointer">
-                Inventarios
+              <li className="border-0 p-0">
+                <Link
+                  href="/inventario"
+                  className="flex p-5 w-full"
+                  activeClassName="bg-slate-800 text-white"
+                >
+                  Inventarios
+                </Link>
               </li>
-              <li className="border-0 border-transparent text-gray-800 cursor-pointer">
-                Materiales
+              <li className="border-0 p-0">
+                <Link
+                  href="/materiales"
+                  className="flex p-5 w-full"
+                  activeClassName="bg-slate-800 text-white"
+                >
+                  Materiales
+                </Link>
               </li>
               <PrivateComponent roles={["ADMIN"]}>
-                <li className="border-0 border-transparent text-gray-800 cursor-pointer">
-                  Usuario
+                <li className="border-0 p-0">
+                  <Link
+                    href="/usuarios"
+                    className="flex p-5 w-full"
+                    activeClassName="bg-slate-800 text-white"
+                  >
+                    Usuario
+                  </Link>
                 </li>
               </PrivateComponent>
             </ul>
